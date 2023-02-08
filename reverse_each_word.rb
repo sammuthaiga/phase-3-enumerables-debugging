bb0b84af3c54d2a9914cb879cd99570c29d414bf
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-02-08 20:18:18 +0300

    Done with practice on debbuging using pry

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,35 @@
-# Write your code here
+# # Write your code here
+# require 'pry'
+
+# def reverse_each_word(sentence)
+#     words = sentence.split
+#     reversed_words = []
+
+#     words.each do |word|
+#         reversed_words << word.reverse
+#     end
+#     reversed_words.join(" ")
+# end
+# binding.pry
+# 0
+
+# puts reverse_each_word("Hello there, and how are you?")
+# def reverse_each_word(sentence)
+#     words = sentence.split
+#     reversed_words = words.map do |word|
+#         word.reverse
+#     end
+#     reversed_words.join(" ")
+# end
+# puts reverse_each_word("Hello there, and how are you?")
+def reverse_each_word(sentence)
+    words = sentence.split
+    reversed_words = words.map(&:reverse)
+    reversed_words.join(" ")
+end
+puts reverse_each_word("Hello there, and how are you?")
+
+def reverse_each_word(sentence)
+    sentence.split.map(&:reverse).join(" ")
+end
+puts reverse_each_word("Hello there, and how are you?")
